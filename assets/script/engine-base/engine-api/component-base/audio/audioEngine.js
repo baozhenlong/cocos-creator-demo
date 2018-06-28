7b190e3f2631b8c04ab8c0b88a62965db139e44e
bzl <mac@macdeMacBook-Pro.local>
2018-06-28 17:14:47 +0800

    整理audio

--- assets/script/engine-base/engine-api/component-base/audio/audioEngine.js
+++ assets/script/engine-base/engine-api/component-base/audio/audioEngine.js
@@ -1,6 +1,3 @@
-//使用AudioEngine播放
-//1---在脚本内定义一个audioClip资源对象
-//2---直接使用cc.audioEngine.play(path, loop, volume)播放
 cc.Class({
     extends: cc.Component,
 
@@ -12,8 +9,12 @@ cc.Class({
     },
 
     onLoad() {
-        //1---play()---播放音频剪辑
-        //参数path（String）---音频剪辑
+        //---使用AudioEngine播放音频
+        //1---在脚本内定义一个audioClip资源对象
+        //2---直接使用cc.audioEngine.play(filePath, loop, volume)播放
+
+        //---audioEngine.play(filePath, loop = false, volume = 1)---播放音频剪辑
+        //参数filePath（String）---音频剪辑
         //可选参数loop（Boolean）---是否循环播放，默认为false
         //参数volume（Number）---音量，默认为1
         //返回值audioID（Number）---可以通过这个audioID来操作这个音频对象
@@ -32,38 +33,54 @@ cc.Class({
         console.log(cc.audioEngine.play(this.egAudio[1], false, 1)); //2
     },
 
-    audioHandle: function() {
-        //2---setLoop(audioId, loop)---设置音频是否循环
+    audioHandle: function () {
+        //---audioEngine.setLoop(audioId, loop)
+        //设置音频是否循环
         //参数audioId（Number）---audio id
         //参数loop（Boolean）---是否循环
         cc.audioEngine.setLoop(this.egAudioId, true);
-        //3---isLoop(audioId)---获取音频的循环状态
+
+        //---audioEngine.isLoop(audioId)
+        //获取音频的循环状态
         //参数audioId（Number）---audio id
         cc.audioEngine.isLoop(this.egAudioId);
-        //4---setVolume(audioId, volume)---设置音量
+
+        //---audioEngine.setVolume(audioId, volume)
+        //设置音量
         //参数audioId（Number）---audio id
-        //参数volume（Number）---音量
+        //参数volume（Number）---音量：[0, 1]
         cc.audioEngine.setVolume(this.egAudioId, 1);
-        //5---getVolume(audioId)---获取音量
+
+        //---audioEngine.getVolume(audioId)
+        //获取音量
         //参数audioId（Number）---audio id
         var volume = cc.audioEngine.getVolume(this.egAudioId);
-        //6---pause(audioId)---暂停指定音频
+
+        //---audioEngine.pause(audioId)
+        //暂停指定音频
         //参数audioId（Number）---audio id        
         cc.audioEngine.pause(this.egAudioId);
-        //7---pauseAll(audioId)---暂停正在播放的所有音频
-        //参数audioId（Number）---audio id        
+
+        //---audioEngine.pauseAll()
+        //暂停正在播放的所有音频     
         cc.audioEngine.pauseAll();
-        //8---resume(audioId)---恢复指定音频
+
+        //---audioEngine.resume(audioId)
+        //恢复指定音频
         //参数audioId（Number）---audio id        
         cc.audioEngine.resume(this.egAudioId);
-        //9---resumeAll(audioId)---恢复正在播放的所有音频
-        //参数audioId（Number）---audio id        
+
+        //---audioEngine.resumeAll()
+        //恢复正在播放的所有音频      
         cc.audioEngine.resumeAll();
-        //10---stop()---停止指定音频   
+
+        //---audioEngine.stop()
+        //停止指定音频   
         cc.audioEngine.stop(this.egAudioId);
-        //11---stopAll(audioId)---停止正在播放的所有音频
-        //参数audioId（Number）---audio id   
-        cc.audioEngine.stopAll(this.egAudioId);
+
+        //---audioEngine.stopAll()
+        //停止正在播放的所有音频
+        cc.audioEngine.stopAll();
     }
 
     // start () {
